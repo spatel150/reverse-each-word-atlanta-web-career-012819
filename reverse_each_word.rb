7238a94c3ed9e6addef62240180fd22c3a56671d
Samidh Patel <spatel13456@gmail.com>
2019-01-11 01:00:40 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,6 +1,3 @@
 def reverse_each_word(sentence1)
-  new_array = sentence1.split("")
-  reverse_array = new_array.each {|rev| rev.reverse!}
-  end
-  return reverse_array.join("")
+  new_array = sentenece1.split
 end
