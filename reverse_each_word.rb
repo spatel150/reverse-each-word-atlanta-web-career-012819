bd882b63bd337a781d7737af195f6c8d8b8db327
Samidh Patel <spatel13456@gmail.com>
2019-01-11 01:12:05 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,5 +1,5 @@
 def reverse_each_word(sentence1)
-  new_array = sentence1.split
+  new_array = sentence1.split 
   new_array2 = []
   new_array.each do |sentence1|
     new_array2 << sentence1.reverse!
